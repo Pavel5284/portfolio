2ac8365c397ac44bcaaed37672e9b72e2d5dbbf2
Cherniakov_Pavel <74683152+Pavel5284@users.noreply.github.com>
2022-06-06 16:20:21 +0200

    Updates

---
+++ precache-manifest.d111041f8869aae77739562f9e173e19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27d9332e561ff92218cfbd725e259f28",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e2c91471416387575de9",
+    "url": "/portfolio/static/css/main.77986f51.chunk.css"
+  },
+  {
+    "revision": "62c755012df5484654a9",
+    "url": "/portfolio/static/js/2.39dfbea6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/portfolio/static/js/2.39dfbea6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2c91471416387575de9",
+    "url": "/portfolio/static/js/main.f2c7ec4b.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
